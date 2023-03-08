65aac7cf93e41ef1bc46e7c40b9090005d1613da
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-03-08 22:30:03 +0100

    add

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const { router } = require("./routes/routes.js");
+
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-app.use(router);
+app.use(morgan('dev'));
 
+app.use(router);
 const port = 3000;
 
 
